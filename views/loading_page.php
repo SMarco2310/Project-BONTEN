ae56aef6541d1ef7e2d09a777e79b549f1066241
EldadOpare <opareeldad@gmail.com>
2025-11-29 16:45:26 +0000

    converting pages to php explore, history and laoding page

---
+++ views/loading_page.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+
+// If user is already logged in, redirect them to appropriate page
+if (isset($_SESSION['user_id'])) {
+    if ($_SESSION['user_type'] === 'manager') {
+        header("Location: manager_dashboard.php");
+    } else {
+        header("Location: user_homepage.php");
+    }
+    exit();
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Loading...</title>
+    <link rel="stylesheet" href="../public/css/loader.css">
+    <link rel="icon" href="../public/assets/bonten.png" type="image/x-icon">
+</head>
+<body>
+    <div class="loader">
+    </div>
+    <script>
+        // Redirect to login page after 3 seconds
+        setTimeout(() => {
+            window.location.href = 'index.php';
+        }, 3000);
+    </script>
+<script src="https://cdn.userway.org/widget.js" data-account="yHxBfPK57z" data-position="3"></script>
+</body>
+</html>
